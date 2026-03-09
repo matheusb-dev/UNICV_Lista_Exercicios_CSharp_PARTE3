dadb2af51fbe2f8f91f0bc3c5d98505d57f8796b
Gabriel-S-Mendes <mesente12@gmail.com>
2026-03-09 14:06:03 -0300

    Add files via upload

---
+++ Bloco 2/Funcionario.cs
@@ -0,0 +1,35 @@
+﻿/**NOME: Gabriel de Souza Mendes
+   RA: 162874-2023
+  
+   NOME: Matheus Bezerra Domingos
+   RA: 166479-2024
+   
+   NOME: Otávio Augusto Zangelmi Costa
+   RA: 159752-2023
+
+   NOME: Luiz Eduardo Marques da Silva 
+   RA:168685-2024
+
+   Exercicios 46, 49, 50, 51: Funcionario.cs
+ 
+ */
+class Funcionario : Pessoa
+{
+    protected double salario;
+
+    public Funcionario(string nome, double salario) : base(nome)
+    {
+        this.salario = salario;
+    }
+
+    public virtual void Trabalhar()
+    {
+        Console.WriteLine("Funcionário trabalhando...");
+    }
+
+    public override void ExibirDados()
+    {
+        base.ExibirDados();
+        Console.WriteLine($"Salário: {salario}");
+    }
+}
---
+++ Bloco 2/Gerente.cs
@@ -0,0 +1,37 @@
+﻿/**NOME: Gabriel de Souza Mendes
+   RA: 162874-2023
+  
+   NOME: Matheus Bezerra Domingos
+   RA: 166479-2024
+   
+   NOME: Otávio Augusto Zangelmi Costa
+   RA: 159752-2023
+
+   NOME: Luiz Eduardo Marques da Silva 
+   RA:168685-2024
+
+   Exercicios 47, 48, 52: Gerente.cs
+ 
+ */
+class Gerente : Funcionario
+{
+    private string departamento;
+
+    public Gerente(string nome, double salario, string departamento)
+        : base(nome, salario)
+    {
+        this.departamento = departamento;
+    }
+
+    public override void Trabalhar()
+    {
+        base.Trabalhar();
+        Console.WriteLine("Gerente gerenciando equipe...");
+    }
+
+    public override void ExibirDados()
+    {
+        base.ExibirDados();
+        Console.WriteLine($"Departamento: {departamento}");
+    }
+}
