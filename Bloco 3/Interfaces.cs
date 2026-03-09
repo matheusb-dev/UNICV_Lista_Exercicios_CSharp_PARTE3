0330529075cd95fbd7d3b9e48c96966fdf09e4b0
Gabriel-S-Mendes <mesente12@gmail.com>
2026-03-09 14:06:38 -0300

    Add files via upload

---
+++ Bloco 3/Interfaces.cs
@@ -0,0 +1,26 @@
+﻿/**NOME: Gabriel de Souza Mendes
+   RA: 162874-2023
+  
+   NOME: Matheus Bezerra Domingos
+   RA: 166479-2024
+   
+   NOME: Otávio Augusto Zangelmi Costa
+   RA: 159752-2023
+
+   NOME: Luiz Eduardo Marques da Silva 
+   RA:168685-2024
+
+   Exercicios 81, 82, 83: Interface.cs
+ 
+ */
+interface IPagamento
+{
+    double Valor { get; set; }
+
+    bool Processar();
+}
+
+interface IRecibo
+{
+    void GerarRecibo();
+}
---
+++ Bloco 3/Pagamentos.cs
@@ -0,0 +1,61 @@
+﻿/**NOME: Gabriel de Souza Mendes
+   RA: 162874-2023
+  
+   NOME: Matheus Bezerra Domingos
+   RA: 166479-2024
+   
+   NOME: Otávio Augusto Zangelmi Costa
+   RA: 159752-2023
+
+   NOME: Luiz Eduardo Marques da Silva 
+   RA:168685-2024
+
+   Exercicios 84–90: Pagamenti.cs
+ 
+ */
+class Pagamento : IPagamento
+{
+    public double Valor { get; set; }
+
+    public Pagamento(double valor)
+    {
+        Valor = valor;
+    }
+
+    public virtual bool Processar()
+    {
+        return true;
+    }
+}
+
+class PagamentoPix : Pagamento, IRecibo
+{
+    public PagamentoPix(double valor) : base(valor) { }
+
+    public override bool Processar()
+    {
+        Console.WriteLine("Pagamento via PIX");
+        return true;
+    }
+
+    public void GerarRecibo()
+    {
+        Console.WriteLine("Recibo PIX gerado");
+    }
+}
+
+class PagamentoCartao : Pagamento, IRecibo
+{
+    public PagamentoCartao(double valor) : base(valor) { }
+
+    public override bool Processar()
+    {
+        Console.WriteLine("Pagamento via Cartão");
+        return true;
+    }
+
+    public void GerarRecibo()
+    {
+        Console.WriteLine("Recibo Cartão gerado");
+    }
+}
