144dfc905dfa032bc0863c5c99d1e76e905ade22
Otávio Zangelmi <otavioazc@gmail.com>
2026-03-10 14:35:42 -0300

    Add files via upload

--- Bloco 3/Ex65.cs
+++ Bloco 3/Ex65.cs
@@ -1,9 +1,42 @@
-List<Forma> formas = new List<Forma>();
+﻿using System;
+using System.Collections.Generic;
 
-formas.Add(new Quadrado());
-formas.Add(new Circulo());
-
-foreach (var f in formas)
+namespace EXSC_
 {
-    Console.WriteLine(f.CalcularArea());
+    internal class Ex65
+    {
+        public abstract class Forma
+        {
+            public abstract void Desenhar();
+        }
+
+        public class Circulo : Forma
+        {
+            public override void Desenhar()
+            {
+                Console.WriteLine("Desenhando círculo");
+            }
+        }
+
+        public class Quadrado : Forma
+        {
+            public override void Desenhar()
+            {
+                Console.WriteLine("Desenhando quadrado");
+            }
+        }
+
+        public void Testar()
+        {
+            List<Forma> formas = new List<Forma>();
+
+            formas.Add(new Circulo());
+            formas.Add(new Quadrado());
+
+            foreach (Forma f in formas)
+            {
+                f.Desenhar();
+            }
+        }
+    }
 }
---
+++ Bloco 3/Ex69.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace EXSC_
+{
+    internal abstract class Ex69
+    {
+        public abstract double CalcularSalario();
+    }
+}
